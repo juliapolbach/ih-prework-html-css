93d9e70d5be379fe2021198f8c5fe69150151f89
juliapolbach <juliapolbach@gmail.com>
2017-01-05 20:42:51 +0100

    Add files via upload

--- mars_rover/rover.js
+++ mars_rover/rover.js
@@ -3,23 +3,122 @@ var myRover = {
   direction: 'N'
 };
 
-function goForward(rover) {
-  switch(rover.direction) {
+//Contador para controlar las veces que el programa pide introducir instrucciones
+var counter = 1;
+
+//Avanzar
+function goForward(myRover) {
+  switch( myRover.direction) {
     case 'N':
-      rover.position[0]++
+       myRover.position[0]++;
       break;
     case 'E':
-      rover.position[1]++
+       myRover.position[1]++;
       break;
     case 'S':
-      rover.position[0]--
+       myRover.position[0]--;
       break;
     case 'W':
-      rover.position[1]--
+       myRover.position[1]--;
       break;
-  };
+  }
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+  console.log("New Rover Position: [" +  myRover.position[0] + ", " +  myRover.position[1] + "] Rover Direction : [" + myRover.direction +"]");
 }
 
-goForward(myRover);
+//Retroceder
+function goBack(myRover) {
+  switch(myRover.direction) {
+    case 'N':
+       myRover.position[0]--;
+      break;
+    case 'E':
+       myRover.position[1]--;
+      break;
+    case 'S':
+       myRover.position[0]++;
+      break;
+    case 'W':
+       myRover.position[1]++;
+      break;
+  }
+  
+  console.log("New Rover Position: [" +  myRover.position[0] + ", " +  myRover.position[1] + "] Rover Direction : [" +myRover.direction +"]");
+}
+
+//Girar derecha
+function goRight(myRover) {
+  switch( myRover.direction) {
+    case 'N':
+      myRover.direction ='E';
+      break;
+    case 'E':
+      myRover.direction ='S';
+      break;
+    case 'S':
+       myRover.direction ='W';
+      break;
+    case 'W':
+       myRover.direction ='N';
+      break;
+  }
+
+  console.log("New Rover Direction: [" + myRover.direction +"] Rover Position: ["+ myRover.position + "]");
+}
+
+//Girar izquierda
+function goLeft(myRover) {
+  switch( myRover.direction) {
+    case 'N':
+       myRover.direction ='W';
+      break;
+    case 'E':
+       myRover.direction ='N';
+      break;
+    case 'S':
+       myRover.direction ='E';
+      break;
+    case 'W':
+       myRover.direction ='S';
+      break;
+  }
+
+    console.log("New Rover Direction: [" + myRover.direction +"] Rover Position: ["+ myRover.position + "]");
+}
+
+//Inicio programa
+  
+console.log("MARS ROVER CONTROL CENTER");
+console.log("#########################");
+console.log("Initial Rover position ["+ myRover.position +"]" );
+console.log("#########################");
+
+//Contador (1 vuelta)
+while (counter <= 1){
+  
+//Input
+myRoverInstr = prompt("Enter command sequence");
+//Convierte script en array
+myRoverArrayInstr = myRoverInstr.split(""); 
+console.log(myRoverArrayInstr);
+
+//Analiza la array y se desplaza
+var arrayLength = myRoverArrayInstr.length;
+for (var i = 0; i < arrayLength; i++) {
+    if (myRoverArrayInstr[i].toLowerCase() === 'f') {
+  goForward(myRover);
+} else if (myRoverArrayInstr[i].toLowerCase() === 'b') {
+  goBack(myRover);
+} else if (myRoverArrayInstr[i].toLowerCase() === 'r') {
+  goRight(myRover);
+} else if (myRoverArrayInstr[i].toLowerCase() === 'l') {
+  goLeft(myRover);
+} else {
+    console.log("Don't be rude with Rover");
+  }
+}
+ counter += 1;
+}
+
+//Posicion Final
+console.log("Final Rover position ["+ myRover.position +"]" );
